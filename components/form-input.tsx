2762a3b57ac61c426ed7365da5a40c2caf60e2bb
zayong <zayong@zayongui-noteubug.local>
2024-04-08 16:24:31 +0900

    router

--- components/form-input.tsx
+++ components/form-input.tsx
@@ -3,18 +3,15 @@ interface FormInputProps {
   placeholder: string;
   required: boolean;
   errors: string[];
+  name: string;
 }
 
-export default function FormInput({
-  type,
-  placeholder,
-  required,
-  errors,
-}: FormInputProps) {
+export default function FormInput({ type, placeholder, required, errors, name }: FormInputProps) {
   return (
     <div className="flex flex-col gap-2">
       <input
         className="bg-transparent rounded-md w-full h-10 focus:outline-none ring-2 focus:ring-4 transition ring-neutral-200 focus:ring-orange-500 border-none placeholder:text-neutral-400"
+        name={name}
         type={type}
         placeholder={placeholder}
         required={required}
