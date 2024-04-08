6eb31f177837c08bdab57e7efe983a131df8e32d
zayong <zayong@zayongui-noteubug.local>
2024-04-08 11:30:27 +0900

    마크업

--- app/page.tsx
+++ app/page.tsx
@@ -4,15 +4,12 @@ export default function Home() {
   return (
     <div className="flex flex-col items-center justify-between min-h-screen p-6">
       <div className="my-auto flex flex-col items-center gap-2 *:font-medium">
-        <span className="text-9xl">🥕</span>
-        <h1 className="text-4xl ">당근</h1>
-        <h2 className="text-2xl">당근 마겟에 어서오세요!</h2>
+        <span className="text-9xl">ㅇ</span>
+        <h1 className="text-4xl ">Carrot</h1>
+        <h2 className="text-2xl">Carrot Market</h2>
       </div>
       <div className="flex flex-col items-center gap-3 w-full">
-        <Link
-          href="/create-account"
-          className="w-full bg-orange-500 text-white text-lg font-medium py-2.5 rounded-md text-center hover:bg-orange-400 transition-colors"
-        >
+        <Link href="/create-account" className="primary-btn text-lg py-2.5">
           시작하기
         </Link>
         <div className="flex gap-2">
